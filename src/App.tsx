f308f01296847b2533aaf6ccb0b68b1ee3b383b1
Imoo <imoo@MacBook-Pro-uzivatela-Imoo.local>
2020-11-16 14:06:02 +0100

    app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
 
+// custom
 import Layout from './components/Layout';
 
+const useStyles = makeStyles(() => ({
+  app: {
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
+    <div className={classes.app}>
       <Layout />
     </div>
   );
