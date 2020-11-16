f1917e7c71ebef90ad2db512a6f6160195594dec
Imoo <imoo@MacBook-Pro-uzivatela-Imoo.local>
2020-11-16 03:53:23 +0100

    App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
-import Layout from './components/Layout';
+import './App.css';
 
-const AppElement = styled.div`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-`;
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <AppElement>
+    <div className="App">
       <Layout />
-    </AppElement>
+    </div>
   );
 }
 
