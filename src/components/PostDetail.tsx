d9a9239a18c4165666ed3271d35f683fd5593ac7
Imoo <imoo@MacBook-Pro-uzivatela-Imoo.local>
2020-11-16 05:06:36 +0100

    posts, postdetail component

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,10 +1,115 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Avatar, Button, Paper } from '@material-ui/core';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { deepOrange } from '@material-ui/core/colors';
 
-function PostDetail() {
+// custom
+import User from './Users';
+
+const useStyles = makeStyles((theme) => ({
+  post: {
+  },
+  title: {
+    margin: theme.spacing(2, 0, 4),
+  },
+  avatar: {
+    display: 'inline-flex',
+    width: theme.spacing(6),
+    height: theme.spacing(6),
+    margin: theme.spacing(0, 2, 4, 0),
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: deepOrange[500],
+  },
+  commentsIcon: {
+    position: 'relative',
+    top: '5px',
+    marginRight: theme.spacing(2),
+  },
+  commentList: {
+    listStyleType: 'none',
+    paddingInlineStart: '0',
+  },
+  commentItem: {
+    padding: theme.spacing(2),
+    margin: theme.spacing(0, 0, 2),
+  },
+  commentItemHeader: {
+    fontSize: '0.8em',
+    padding: theme.spacing(0, 0, 1),
+  },
+}));
+
+const PostDetail = (props: any) => {
+  const classes = useStyles();
+  const [post] = useState(props.post);
+  const [comments, setComments] = useState([]);
+  const [user, setUser] = useState<typeof User | null>(props.post.userId);
+  const [avatar, setAvatar] = useState<string | null>(null);
+
+  function onTrigger(event: any) {
+    props.parentCallback(null);
+    event.preventDefault();
+  }
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
+      .then(res => {
+        setComments(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+
+    axios.get(`https://jsonplaceholder.typicode.com/users/${user}`)
+      .then(res => {
+        setUser(res.data);
+        setAvatar(res.data.name.charAt(0));
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, [post]);
+
   return (
-    <>
-      <div>Hello 👋, I am a PostDetail component.</div>
-    </>
+    <div className={classes.post}>
+      <Typography variant="h3" color="inherit" className={classes.title}>
+        {post.title}
+      </Typography>
+      {
+        user !== null ?
+          <div>
+            <Avatar className={classes.avatar}>{avatar}</Avatar>
+            <span>{user.name}</span>
+          </div>
+          : null
+      }
+      <div>{post.body}</div>
+      <h3><ChatOutlinedIcon className={classes.commentsIcon}></ChatOutlinedIcon>Comments</h3>
+      {
+        comments.length !== 0 ?
+          <ul className={classes.commentList}>
+            {
+              comments.map((comment: any) => (
+                <li key={comment.id}>
+                  <Paper className={classes.commentItem}>
+                    <Typography className={classes.commentItemHeader}>
+                      <strong>{comment.name}</strong>, <i><a href={`mailto:${comment.email}`}>{comment.email}</a></i>
+                    </Typography>
+                    <Typography>{comment.body}</Typography>
+                  </Paper>
+                </li>
+              ))
+            }
+          </ul>
+          : null
+      }
+      <Button variant="contained" color="primary" onClick={onTrigger}>
+        Go Back
+      </Button>
+    </div>
   );
 };
 
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { DataGrid, ColDef, RowParams } from '@material-ui/data-grid';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Posts() {
+// custom
+import PostDetail from './PostDetail';
+
+export type Post = {
+  userId: number,
+  id: number,
+  title: string;
+  body: string;
+};
+
+const columns: ColDef[] = [
+  { field: 'id', headerName: '#', width: 70 },
+  { field: 'title', headerName: 'Title', width: 500 },
+];
+
+const useStyles = makeStyles((theme) => ({
+  postList: {
+  },
+}));
+
+const PostList = () => {
+  const classes = useStyles();
+  const [posts, setPosts] = useState([]);
+  const [postDetail, setPostDetail] = useState<Post | null>(null);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        setPosts(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, []);
+
+  function handleShowPostDetail(clickedPost: any) {
+    setPostDetail(clickedPost.data);
+  }
+
+  function handleCallback(postDetail: Post) {
+    setPostDetail(postDetail);
+  }
+
   return (
-    <>
-      <div>Hello 👋, I am a Posts component.</div>
-    </>
+    <div className={classes.postList}>
+      { postDetail === null ?
+        <div style={{ width: 700, height: 400 }}>
+          <DataGrid
+            rows={posts}
+            columns={columns}
+            pageSize={10}
+            onRowClick={(param: RowParams) => handleShowPostDetail(param)}
+          />
+        </div>
+        :
+        <PostDetail post={postDetail} parentCallback={handleCallback} />
+      }
+    </div>
   );
 };
 
-export default Posts;
+export default PostList;
