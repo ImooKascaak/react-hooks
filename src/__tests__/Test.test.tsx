7ec65a8e0014f6c4b5050b0bfa36d356c91def0c
Imoo <imoo@MacBook-Pro-uzivatela-Imoo.local>
2020-11-17 09:33:41 +0100

    Jest, Enzyme testing env configuration

---
+++ src/__tests__/Test.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import Layout from '../components/Layout';
+
+describe('<Layout />', () => {
+  it('renders posts/users button', () => {
+    // const element = shallow(<Layout />);
+    // expect(element.find('.btn-posts')).to.exist;
+  });
+
+  it('', () => {
+  });
+
+  it('', () => {
+  });
+});
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -48,31 +48,31 @@ export default function Layout() {
       <CssBaseline />
       {/* TODO: background */}
       {/* <div className={classes.background}> */}
-        <AppBar position="fixed">
-          <Toolbar className={classes.toolbar}>
-            <img className={classes.icon} src="https://www.smartsuppcdn.com/assets/img/logo/smartsupp.svg" alt="" width="80px" height="50px" />
-            <Typography variant="h4" color="inherit" noWrap>
-              {resourceType === 'posts' ? 'Posts' : 'Users'}
-            </Typography>
-            <div className={classes.grow} />
-            {resourceType !== 'posts' ?
-              <Button variant="contained" color="secondary" onClick={() => setResourceType('posts')}>
-                Posts
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          <img className={classes.icon} src="https://www.smartsuppcdn.com/assets/img/logo/smartsupp.svg" alt="" width="80px" height="50px" />
+          <Typography variant="h4" color="inherit" noWrap>
+            {resourceType === 'posts' ? 'Posts' : 'Users'}
+          </Typography>
+          <div className={classes.grow} />
+          {resourceType !== 'posts' ?
+            <Button variant="contained" color="secondary" onClick={() => setResourceType('posts')}>
+              Posts
             </Button>
-              :
-              <Button variant="contained" color="secondary" onClick={() => setResourceType('users')}>
-                Users
+            :
+            <Button variant="contained" color="secondary" onClick={() => setResourceType('users')}>
+              Users
             </Button>
-            }
-          </Toolbar>
-        </AppBar>
-        <main>
-          <div className={classes.content}>
-            <Container maxWidth="md">
-              {resourceType === 'posts' ? <Posts /> : <Users />}
-            </Container>
-          </div>
-        </main>
+          }
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.content}>
+          <Container maxWidth="md">
+            {resourceType === 'posts' ? <Posts /> : <Users />}
+          </Container>
+        </div>
+      </main>
       {/* </div> */}
     </React.Fragment>
   );
