fecd16ca63f578a731de1f190f1f1c746c3058c1
Imoo <imoo@MacBook-Pro-uzivatela-Imoo.local>
2020-11-17 11:04:57 +0100

    simple unit tests

--- src/__tests__/Test.test.tsx
+++ src/__tests__/Test.test.tsx
@@ -1,17 +1,47 @@
 import React from "react";
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import { render, waitFor, cleanup } from '@testing-library/react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import mockedAxios from 'axios';
+
+import App from '../App';
 import Layout from '../components/Layout';
 
-describe('<Layout />', () => {
-  it('renders posts/users button', () => {
-    // const element = shallow(<Layout />);
-    // expect(element.find('.btn-posts')).to.exist;
+afterEach(cleanup);
+
+// test('mocking axios request', async () => {
+//   const data = {
+//     data: [
+//       {
+//         id: 1,
+//         title: 'title 1'
+//       },
+//       {
+//         id: 2,
+//         title: 'title 2'
+//       },
+//       {
+//         id: 3,
+//         title: 'mocked title'
+//       }]
+//   };
+//   mockedAxios.get.mockResolvedValueOnce(data);
+//   const { getByText } = render(<Layout />);
+//   await waitFor(() => {
+//     expect(getByText('mocked title'));
+//   });
+// });
+
+describe('<App />', () => {
+  it('App renders <Layout /> component', () => {
+    const element = shallow(<App />);
+    expect(element.find(Layout)).to.have.lengthOf(1);
   });
 
-  it('', () => {
-  });
-
-  it('', () => {
+  it('Layout renders AppBar', () => {
+    const element = shallow(<Layout />);
+    expect(element.find(AppBar)).to.have.lengthOf(1);
   });
 });
